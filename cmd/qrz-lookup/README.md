c17b88419d1bd71063af8ca921c69f120f3f981a
Chris Keller <xylo04@gmail.com>
2020-05-20 12:33:50 -0600

    Add CLI demo

---
+++ cmd/qrz-lookup/README.md
@@ -0,0 +1,8 @@
+Command-line utility to run a QRZ.com callsign query. This is mostly a demo of 
+the OpenAPI generated client.
+
+## Usage
+
+```shell
+qrz-lookup -username $QRZUSER -password $QRZPASS -lookup K0SWE
+```
---
+++ cmd/qrz-lookup/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/xylo04/qrz-api"
+	"os"
+)
+
+func main() {
+	user := flag.String("username", "", "QRZ.com login name")
+	pw := flag.String("password", "", "QRZ.com password")
+	call := flag.String("lookup", "", "The callsign to look up")
+	flag.Parse()
+	if *user == "" || *pw == "" || *call == "" {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	lookupResp, err := qrz.Lookup(user, pw, call)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	if lookupResp.Callsign == (qrz.Callsign{}) {
+		fmt.Println(*call, "was not found")
+	} else {
+		fmt.Println(*call, "is", lookupResp.Callsign.Fname, lookupResp.Callsign.Name)
+	}
+}
